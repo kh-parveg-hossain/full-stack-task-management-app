b2ac5100e26fa49a49f35d18cc3a11b461c415e2
kh-parveg-hossain <parevgkh64@gmail.com>
2025-01-09 21:57:22 +0530

    c

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -27,15 +27,15 @@ const Footer = () => {
             <div className="footer-content-right">
                 <h2>GET IN TOUCH</h2>
                 <ul>
-                    <li>+94 765489545</li>
-                    <li>dulanjalisenarathna93@gmail.com</li>
+                    <li>+91 123456789</li>
+                    <li>parevgkh64@gmail.com</li>
                 </ul>
             </div>
            
         </div>
         <hr />
         <p className="footer-copyright">
-            Copyright 2024 &copy; Dulanjali - All Right Reserved.
+            Copyright 2024 &copy; parveg - All Right Reserved.
         </p>
     </div>
   )
