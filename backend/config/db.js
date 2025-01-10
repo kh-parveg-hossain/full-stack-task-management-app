109e187ac44775cab4e1ec84e4d76a6e1af4d88b
kh-parveg-hossain <parevgkh64@gmail.com>
2025-01-10 15:28:34 +0530

    UPDATE

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://parveg:rV2WLY9WyRbAzdEn@cluster0.n19ei.mongodb.net/food-del').then(()=>{
+    await mongoose.connect(process.env.MONGO_URL).then(()=>{
        console.log('DB connected') ;
     })
 }
